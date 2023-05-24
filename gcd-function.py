5e99faa2825ac2b37159f11554cba6e777fee13a
brokstork <ganj.169@mail.ru>
2023-05-24 17:28:28 +0300

    hey

--- gcd-function.py
+++ gcd-function.py
@@ -2,9 +2,7 @@ def find_gcd(a, b):
     r = a % b
     if r == 0:
         print('НОД -', b)
-    elif r == 1:
-        print('НОД - 1')
     else:
         a = b; b = r
         find_gcd(a, b)
-find_gcd(56, 32)
+find_gcd(13, 12)
